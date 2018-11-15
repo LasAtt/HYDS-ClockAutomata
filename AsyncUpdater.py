4fe6ea7421977953e20db2fa46eb35a8af5a1ebd
Atte Lassila <atte.lassila@relexsolutions.com>
2018-11-15 19:08:46 +0200

    files

---
+++ AsyncUpdater.py
@@ -0,0 +1,33 @@
+import threading
+import time
+
+class AsyncUpdater:
+
+  def __init__(self, func, visibleFunc, interval):
+    self.func = func
+    self.visibleFunc = visibleFunc
+    self.interval = interval
+    self.on = False
+    self.visible = False
+    threading.Thread(target = self.updater).start()
+
+  def updater(self):
+    while(True): 
+      if self.on:
+        self.func()
+        if self.visible:
+          print self.interval
+          self.visibleFunc()
+        time.sleep(self.interval)
+
+  def start(self):
+    self.on = True
+
+  def stop(self):
+    self.on = False
+
+  def show(self):
+    self.visible = True
+
+  def hide(self):
+    self.visible = False
